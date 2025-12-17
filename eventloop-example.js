64077e8ca6c69e41546fceb438739d6000606bad
Raj Kumar <145471187+RajKumarydv@users.noreply.github.com>
2025-12-17 22:32:14 +0530

    Delete eventloop-example.js

--- eventloop-example.js
+++
@@ -1,9 +0,0 @@
-console.log("Start");
-
-setTimeout(() => {
-  console.log("This runs after 5 seconds");
-}, 5000);
-
-console.log("End");
-console.log("Today I learned what Node.js is and how it works!");
-console.log("Node.js allows JavaScript to run outside the browser.");
